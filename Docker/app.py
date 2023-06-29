d0a011b8608d18e4f82043f45285995486b017e1
PeterHaislund <95341682+PeterHaislund@users.noreply.github.com>
2023-06-29 13:25:37 +0200

    Support for config file

---
+++ Docker/app.py
@@ -0,0 +1,117 @@
+import os.path
+import os          
+import subprocess 
+import sys                                                                                                      
+import re  
+import time  
+from datetime import datetime
+import json   
+import paho.mqtt.client as mqtt      
+import traceback
+from config import Config
+  
+##########################
+### TEST STUB FOR MQTT
+##########################
+class mqtt_stub:
+  def __init__(self):
+    print("No mqtt broker configured - using test stub")
+    
+  def username_pw_set(self, username, password):
+    print("Setting user/pass: " + username + " | " + password)
+
+  def connect(self, broker_server, broker_server_port , timeout):
+    print("Connecting to test stub " + broker_server + ":" + str(broker_server_port) + " | " + str(timeout))
+    
+  def loop_start(self) :
+    print("loop_start")
+    
+  def publish(self, topic, value):
+    print("publishing " + topic + " | " + str(value))
+    
+  def loop_stop(self):
+    print("loop_stop")    
+
+##########################
+### APP FUNCTIONS
+##########################
+def query_heatpump(heatpump):
+    #cmd = "/app/AlsavoCtrl -s "+ heatpump["serial"] + " -l " + heatpump["password"]
+    #response = os.popen(cmd).read()
+    
+    #with open("C:/temp/alsavo.txt") as f:
+    #    response = f.readlines()
+ 
+    response = subprocess.check_output(["/app/AlsavoCtrl", "-s", heatpump["serial"], "-l", heatpump["password"]], text=True, universal_newlines=True)
+ 
+    return response
+
+##########################
+### APP CODE
+##########################
+with open('/data/alsavo_status.log', 'a') as sys.stdout:
+    try:
+        config_loader = Config()
+        config = config_loader.load_config()
+
+        mqtt_config = config["mqtt"]
+        
+        broker_server = mqtt_config["server"]
+        broker_server_port = mqtt_config["server_port"]
+        broker_user = mqtt_config["server_user"]
+        broker_password = mqtt_config["server_password"]
+        
+        if not (broker_server == "None") and not (broker_server is None):
+            client = mqtt.Client()    
+        else:
+            client = mqtt_stub()    
+        
+        if not (broker_user == "None") and not (broker_user is None):
+          client.username_pw_set(broker_user, password=broker_password)
+         
+        client.connect(broker_server, int(broker_server_port) , 60) 
+        client.loop_start()  
+
+        while True:
+            try: 
+                print(datetime.now().strftime("%Y-%m-%d, %H:%M:%S") + " Publishing status")
+      
+                for heatpump in config["heatpumps"]: 
+                    response = query_heatpump(heatpump)
+                                    
+                    for line in response.split('\n'):
+                        m = re.match('(.*)=(.*)', line)     
+                        if m:                
+                            inputObject = json.loads(m.group(2))
+                            inputType = inputObject["type"]
+                            inputIndex = inputObject["index"]
+                            inputValue = inputObject["value"]
+                                            
+                            config_topics = config_loader.find_topics(heatpump["topics"], inputType, inputIndex)
+                            
+                            for config_topic in config_topics:
+                                for mapping in config_topic["mappings"]:
+                                    if not mapping is None:                        
+                                        if mapping["type"] == "replace":
+                                            for mappingValue in mapping["replacements"]:
+                                                if str(inputValue) == (mappingValue["from"]):
+                                                    inputValue = mappingValue["to"]
+                                        if mapping["type"] == "eval":
+                                            value = inputValue;
+                                            inputValue = eval(mapping["formula"])
+                                        
+                                print(datetime.now().strftime("%Y-%m-%d, %H:%M:%S") + " Publishing: " + config_topic["topic"] + " : " + str(inputValue))
+                                client.publish(config_topic["topic"], str(inputValue)) 
+                
+            except Exception as inst:
+                print(datetime.now().strftime("%Y-%m-%d, %H:%M:%S").join(traceback.TracebackException.from_exception(inst).format()))      
+
+            #Flush log file
+            sys.stdout.flush()
+                   
+            time.sleep(int(config["publish_interval"]))
+
+        client.loop_stop()  
+      
+    except Exception as inst:
+        print(datetime.now().strftime("%Y-%m-%d, %H:%M:%S").join(traceback.TracebackException.from_exception(inst).format()))   
---
+++ Docker/config.py
@@ -0,0 +1,135 @@
+import json
+import os.path
+import os  
+
+class Config:
+    config_file = "/data/config.json"
+    #config_file = "c:/temp/config.json"
+    
+    def load_config(self):
+        self.validate_config()
+        
+        with open(self.config_file, 'r') as openfile:
+            config = json.load(openfile)
+    
+        return config
+    
+    def find_topics(self, topic_list, entry_type, index):
+        topics = []
+        
+        for topic in topic_list:
+            inputType = topic["type"]
+            inputIndex = topic["index"]
+          
+            if inputType == entry_type and inputIndex == index:
+                topics.append(topic)
+    
+        return topics
+        
+    def validate_config(self):
+        if not os.path.isfile(self.config_file):
+            # Generate default config based on ENV variables if config file doesn't exist
+            config = {
+                "publish_interval": os.getenv("publish_interval"),
+                "log_size": os.getenv("log_size"),
+                "mqtt": {
+                    "server": os.getenv("mqtt_server"),
+                    "server_port": os.getenv("mqtt_server_port"),
+                    "server_user": os.getenv("mqtt_server_user"),
+                    "server_password": os.getenv("mqtt_server_password")
+                },
+                "heatpumps": [
+                    {
+                        "serial": os.getenv("alsavo_serial"),
+                        "password": os.getenv("alsavo_password"),
+                        "topics": [                            
+                            {
+                                "type": "status",
+                                "index": 16,
+                                "topic": "stats/pool/water_in_temp",
+                                "mappings": []
+                            },
+                            {
+                                "type": "status",
+                                "index": 17,
+                                "topic": "stats/pool/water_out_temp",
+                                "mappings": []
+                            },
+                            {
+                                "type": "status",
+                                "index": 18,
+                                "topic": "stats/pool/ambient_temp",
+                                "mappings": []
+                            },
+                            {
+                                "type": "status",
+                                "index": 22,
+                                "topic": "stats/pool/fan_speed",
+                                "mappings": []
+                            },
+                            {
+                                "type": "status",
+                                "index": 22,
+                                "topic": "stats/pool/heating",
+                                "mappings": [
+                                    {
+                                        "type": "eval",
+                                        "formula": "value>0"
+                                    },
+                                    {
+                                        "type": "replace",
+                                        "replacements": [
+                                            {
+                                                "from": "True",
+                                                "to": "on"
+                                            },
+                                            {
+                                                "from": "False",
+                                                "to": "off"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "config",
+                                "index": 16,
+                                "topic": "stats/pool/power_mode",
+                                "mappings": [
+                                    {
+                                        "type": "replace",
+                                        "replacements": [
+                                            {
+                                                "from": "0",
+                                                "to": "Silent"
+                                            },
+                                            {
+                                                "from": "1",
+                                                "to": "Smart"
+                                            },
+                                            {
+                                                "from": "2",
+                                                "to": "Powerful"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "config",
+                                "index": 1,
+                                "topic": "stats/pool/target_temp",
+                                "mappings": [
+                                    {
+                                        "type": "eval",
+                                        "formula": "value/10"
+                                    }
+                                ]
+                            },
+                        ]
+                    }
+                ]			
+              }
+              
+            with open(self.config_file, 'a') as outfile:
+              json.dump(config, outfile, indent=4)
